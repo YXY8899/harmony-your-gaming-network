cb1159db05aca5c27f14e16dba1cabf17072b7af
YXY8899 <yeoxinyu9@gmail.com>
2026-04-02 22:04:30 +0800

    Fix BrowserRouter basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/harmony-your-gaming-network">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
